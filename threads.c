41419c159c8c67bec2b53f06b076bcbb4f88da4b
Agurato <mail.vmonot@gmail.com>
2016-11-23 15:39:04 +0100

    file parsing and realloc

--- threads.c
+++ threads.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <string.h>
 
 void* task0(void* arg);
 void* task1(void* arg);
@@ -15,21 +16,76 @@ typedef struct {
 	int next;
 } TaskInfo;
 
-sem_t* mutexList;
+sem_t* mutexList = NULL;
 
 int main(int argc, char const *argv[]) {
-	TaskInfo* taskInfo;
-	
-	pthread_t tasks[5];
-	
-	mutexList = malloc(5*sizeof(sem_t));
+	TaskInfo* taskInfo = NULL;
+	pthread_t* tasks = NULL;
+
+	FILE* taskListFile;
+	char* line = NULL;
+	size_t length = 0;
+	ssize_t read;
+
+	char* lineToken;
+	char* taskToken;
+	char* lineTasks;
+	int taskNb = 0;
+	int lineNb = 1;
+
+	taskListFile = fopen("taskList.txt", "r");
+	if(taskListFile == NULL) {
+		return 1;
+	}
+
+	while((read = getline(&line, &length, taskListFile)) != -1) {
+		lineNb++;
+
+		if(line[strlen(line)-1] == '\n') {
+			line[strlen(line)-1] = '\0';
+		}
+		lineToken = strtok(line, ":");
+		
+		lineNb = lineToken[0] - '0';
+		
+		lineTasks = strtok(NULL, ":");
+
+		while(lineToken != NULL) {
+			lineToken = strtok(NULL, ":");
+		}
+
+		taskToken = strtok(lineTasks, "-");
+		while(taskToken != NULL) {
+			taskNb ++;
+			/* printf("%d -> %s\n", taskNb, taskToken); */
+
+			tasks = realloc(tasks, taskNb*sizeof(pthread_t));
+			mutexList = malloc(taskNb*sizeof(sem_t));
+			taskInfo = malloc(taskNb*sizeof(TaskInfo));
+
+			taskToken = strtok(NULL, "-");
+		}
+	}
+
+	printf("%d lines, %d tasks\n", lineNb, taskNb);
+
+	fclose(taskListFile);
+	if(line) {
+		free(line);
+	}
+
+	/*
+	tasks = malloc(taskNb*sizeof(pthread_t));
+	mutexList = malloc(taskNb*sizeof(sem_t));
+	taskInfo = malloc(taskNb*sizeof(TaskInfo));
+	*/
+
 	sem_init(&mutexList[0], 0, 1);
 	sem_init(&mutexList[1], 0, 1);
 	sem_init(&mutexList[2], 0, 0);
 	sem_init(&mutexList[3], 0, 0);
 	sem_init(&mutexList[4], 0, 1);
 
-	taskInfo = malloc(5*sizeof(TaskInfo));
 	taskInfo[0].nb = 0;
 	taskInfo[0].next = 0;
 	taskInfo[1].nb = 1;
